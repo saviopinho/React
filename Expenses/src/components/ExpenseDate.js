dcdb8b7aca32445d3b26396179fea85abea58b9c
SAVIO PINHO NUNES <savio.pinho@hotmail.com>
2022-10-28 13:48:41 -0300

    new-expenses

---
+++ Expenses/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css';
+
+function ExpenseDate({ date }) {
+    const day = date.toLocaleString('pt-BR', { day: '2-digit' });
+    const month = date.toLocaleString('pt-BR', { month: 'long' });
+    const year = date.getFullYear();
+
+    return (
+        <div className="expense-date">
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
